d0aa6c2441095c46ada51886cc156fbc4e0ecd3c
stwind <stwindfy@gmail.com>
2015-02-01 22:11:54 +0800

    Add webpack dist configs and tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,34 @@
 'use strict';
 
 var gulp = require('gulp');
+var runSequence = require('run-sequence');
 var $ = require('gulp-load-plugins')();
 var $log = $.util.log;
 
 var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var webpackConfig = require('./webpack.config.js');
+var webpackDistConfig = require('./webpack.dist.config.js');
 
-gulp.task('clean', require('del').bind(null, ['.tmp', 'dist']));
+var DEST = 'dist';                         // The build output folder
+
+gulp.task('clean', require('del').bind(null, [DEST]));
+
+gulp.task('copy', function() {
+  return gulp.src('src/index.html')
+    .pipe(gulp.dest(DEST))
+    .pipe($.size({title: 'copy'}));
+});
 
 gulp.task('webpack', function () {
   return gulp.src('src/scripts/main.js')
-  .pipe($.webpack(webpackConfig))
-  .pipe($.size())
-  .pipe(gulp.dest('.tmp/scripts'));
+  .pipe($.webpack(webpackDistConfig))
+  .pipe(gulp.dest(DEST + '/assets'))
+  .pipe($.size({ title: 'webpack' }));
+});
+
+gulp.task('build', ['clean'], function(cb) {
+  runSequence(['webpack', 'copy'], cb);
 });
 
 gulp.task('webpack-dev-server', function (callback) {
---
+++ webpack.dist.config.js
@@ -0,0 +1,67 @@
+/*
+ * Webpack development server configuration
+ *
+ * This file is set up for serving the webpak-dev-server, which will watch for changes and recompile as required if
+ * the subfolder /webpack-dev-server/ is visited. Visiting the root will not automatically reload.
+ */
+'use strict';
+var webpack = require('webpack'),
+    path = require('path');
+
+module.exports = {
+
+  output: {
+    publicPath: '/assets/',
+    filename: "[name].js"
+  },
+
+  cache: true,
+  debug: false,
+  devtool: false,
+  entry: [
+      './src/scripts/main.js'
+  ],
+
+  stats: {
+    colors: true,
+    reasons: true
+  },
+
+  resolve: {
+    extensions: ['', '.js'],
+    alias: {
+      styles: path.resolve(__dirname, "src/styles")
+    }
+  },
+
+  module: {
+    preLoaders: [{
+      test: '\\.js$',
+      exclude: 'node_modules',
+      loader: 'jshint'
+    }],
+    loaders: [{
+      test: /\.js$/,
+      loaders: ['react-hot','jsx?harmony']
+    }, {
+      test: /\.scss/,
+      loader: "style!css!sass?outputStyle=expanded&" +
+          "includePaths[]=" +
+            (path.resolve(__dirname, "./node_modules"))
+    }, {
+      test: /\.css$/,
+      loader: 'style-loader!css-loader'
+    }, {
+      test: /\.(png|jpg)$/,
+      loader: 'url-loader?limit=8192'
+    }]
+  },
+
+  plugins: [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.AggressiveMergingPlugin()
+  ]
+
+};
